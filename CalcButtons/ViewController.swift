5c845a8a4fde7c17870b9f8e332ff9493c4fcbed
DonMag <demagnus@ra.rockwell.com>
2020-03-30 12:49:11 -0400

    testing

--- CalcButtons/ViewController.swift
+++ CalcButtons/ViewController.swift
@@ -24,6 +24,18 @@ struct CalcButton {
 
 class ViewController: UIViewController {
 	
+	let plainButton: UIButton = {
+		let v = UIButton()
+		v.translatesAutoresizingMaskIntoConstraints = false
+		return v
+	}()
+	
+	var cButton: CalcButton!
+	var rButton: RoundButton!
+	
+	let bFont: UIFont = UIFont.systemFont(ofSize: 28.0)
+	let sFont: UIFont = UIFont.systemFont(ofSize: 20.0)
+	
 	let inputLabel: UILabel = {
 		let v = UILabel()
 		v.textColor = .white
@@ -174,7 +186,76 @@ class ViewController: UIViewController {
 		
 		// just so we can see the frame of the inputLabel
 		inputLabel.backgroundColor = .gray
+
+		cButton = CalcButton(normalTitle: "7",  selectedTitle: "", foreColor: .white, backColor: colorB)
 		
+		rButton = RoundButton()
+		rButton.translatesAutoresizingMaskIntoConstraints = false
+		rButton.backgroundColor = cButton.backColor
+//		rButton.setTitleColor(cButton.foreColor, for: .normal)
+//		rButton.setTitle(cButton.normalTitle, for: .normal)
+		
+		normAttributes = [
+			.foregroundColor : cButton.foreColor,
+			.underlineStyle: 0,
+			.font : bFont,
+		]
+		
+		highAttributes = [
+			.foregroundColor : UIColor.lightGray,
+			.underlineStyle: 0,
+			.font : bFont,
+		]
+		
+		let attNorm = NSAttributedString(string: cButton.normalTitle, attributes: normAttributes)
+		let attHigh = NSAttributedString(string: cButton.normalTitle, attributes: highAttributes)
+		
+		rButton.setAttributedTitle(attNorm, for: .normal)
+		rButton.setAttributedTitle(attHigh, for: .highlighted)
+
+		plainButton.titleLabel?.font = font
+		plainButton.setTitle("A", for: .normal)
+		plainButton.setTitleColor(fColorA, for: .normal)
+		plainButton.backgroundColor = colorA
+		
+		view.addSubview(plainButton)
+		view.addSubview(rButton)
+		
+		NSLayoutConstraint.activate([
+
+			plainButton.topAnchor.constraint(equalTo: g.topAnchor, constant: 60.0),
+			plainButton.leadingAnchor.constraint(equalTo: g.leadingAnchor, constant: 80.0),
+			plainButton.widthAnchor.constraint(equalToConstant: 60.0),
+			plainButton.heightAnchor.constraint(equalToConstant: 60.0),
+			
+			rButton.topAnchor.constraint(equalTo: plainButton.bottomAnchor, constant: 8.0),
+			rButton.leadingAnchor.constraint(equalTo: plainButton.leadingAnchor),
+			rButton.widthAnchor.constraint(equalTo: plainButton.widthAnchor),
+			rButton.heightAnchor.constraint(equalTo: plainButton.heightAnchor),
+			
+		])
+
+		#if ignore
+		#if useOverrideButtonShapes //-----------------
+		normalAttributes = [ .foregroundColor : tlc, .underlineStyle: 0, .font: ft ];
+		let normalAttString = NSAttributedString(string: t.Title, attributes: normalAttributes);
+		btn.setAttributedTitle(normalAttString, for: .normal);
+		if(t.hlTitle.count > 0)
+		{
+			selectedAttributes = [ .foregroundColor : hlc, .underlineStyle: 0, .font: ft ];
+			let selectedAttString = NSAttributedString(string: t.hlTitle, attributes: selectedAttributes);
+			btn.setAttributedTitle(selectedAttString, for: .selected);
+			btn.setAttributedTitle(selectedAttString, for: .highlighted);
+		}
+		#else //-----------------
+		btn.titleLabel?.adjustsFontSizeToFitWidth = true;
+		btn.setTitle(t.Title, for: .normal);
+		btn.setTitle(t.hlTitle, for: .selected);
+		btn.titleLabel?.font = ft;
+		btn.setTitleColor(tlc,for:.normal);
+		btn.setTitleColor(hlc,for:.selected);
+		#endif //-----------------
+		#endif
 	}
 	
 	@objc func btnTapped(_ sender: Any?) -> Void {
@@ -190,6 +271,52 @@ class ViewController: UIViewController {
 		}
 		
 	}
-	
+
+	override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+		super.viewWillTransition(to: size, with: coordinator)
+		if size.width < size.height {
+			plainButton.titleLabel?.font = bFont
+			
+			normAttributes = [
+				.foregroundColor : cButton.foreColor,
+				.underlineStyle: 0,
+				.font : bFont,
+			]
+			
+			highAttributes = [
+				.foregroundColor : UIColor.lightGray,
+				.underlineStyle: 0,
+				.font : bFont,
+			]
+			
+			let attNorm = NSAttributedString(string: cButton.normalTitle, attributes: normAttributes)
+			let attHigh = NSAttributedString(string: cButton.normalTitle, attributes: highAttributes)
+			
+			rButton.setAttributedTitle(attNorm, for: .normal)
+			rButton.setAttributedTitle(attHigh, for: .highlighted)
+
+		} else {
+			plainButton.titleLabel?.font = sFont
+
+			normAttributes = [
+				.foregroundColor : cButton.foreColor,
+				.underlineStyle: 0,
+				.font : sFont,
+			]
+			
+			highAttributes = [
+				.foregroundColor : UIColor.lightGray,
+				.underlineStyle: 0,
+				.font : sFont,
+			]
+			
+			let attNorm = NSAttributedString(string: cButton.normalTitle, attributes: normAttributes)
+			let attHigh = NSAttributedString(string: cButton.normalTitle, attributes: highAttributes)
+			
+			rButton.setAttributedTitle(attNorm, for: .normal)
+			rButton.setAttributedTitle(attHigh, for: .highlighted)
+			
+		}
+	}
 }
 
